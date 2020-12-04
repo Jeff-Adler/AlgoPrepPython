3ec36d8500445d0f9436419cbfc87df10d697afb
Jeff-Adler <Jeffm.adler@gmail.com>
2020-12-04 11:48:29 -0500

    flipping an image

---
+++ Leetcode Problems/FlippingAnImage.py
@@ -0,0 +1,12 @@
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        inversion_table = {0 : 1, 1 : 0}
+                            
+        def invertRow(row):
+            return [inversion_table[cell] for cell in row[::-1]]
+            
+        inverted_A = [invertRow(row) for row in A]
+        
+        # inverted_A = map(invertRow, A)
+        
+        return inverted_A
