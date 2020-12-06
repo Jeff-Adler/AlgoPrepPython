3a5bff1fdba80548a58c495c0cfa95d1b23bae3f
Jeff-Adler <Jeffm.adler@gmail.com>
2020-12-06 09:52:32 -0500

    replace elements, simplified

--- Leetcode Problems/ReplaceElementswithGreatestElement.py
+++ Leetcode Problems/ReplaceElementswithGreatestElement.py
@@ -1,12 +1,22 @@
+# class Solution:
+#     def replaceElements(self, arr: List[int]) -> List[int]:
+
+#         transformed_arr = []
+
+#         while len(arr) - 1:
+#             item = arr.pop(0)
+
+#             transformed_arr.append(max(arr))
+
+#         transformed_arr.append(-1)
+
+#         return transformed_arr
+
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
 
-        transformed_arr = []
-
-        while len(arr) - 1:
-            item = arr.pop(0)
-
-            transformed_arr.append(max(arr))
+        transformed_arr = [max(arr[index + 1:]) for index,
+                           num in enumerate(arr) if index != len(arr) - 1]
 
         transformed_arr.append(-1)
 
